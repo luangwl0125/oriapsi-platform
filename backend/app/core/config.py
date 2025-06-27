32b469e0712f94e7fceccae2d7a39286f0120a85
LUAN GAMA <luan.gama.psicologo@gmail.com>
2025-06-27 07:52:26 -0300

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,11 +1,15 @@
+# app/core/config.py
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_url: str
-    stripe_secret_key: str
+    stripe_api_key: str
     stripe_webhook_secret: str
 
-    class Config:
-        env_file = "../.env"
+    model_config = {
+        "env_file": ".env",
+        # se tiver campos de modelo usando orm_mode, substitua por from_attributes:
+        "from_attributes": True,
+    }
 
 settings = Settings()
